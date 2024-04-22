0fba10db5faaa6135ddc5c79807603809cfaca3e
gomdopi <io.gomdopi@gmail.com>
2024-04-22 13:38:29 -0700

    solving gas-station again

--- top-interview-150-problems/array-string/gas-station/solution.ts
+++ top-interview-150-problems/array-string/gas-station/solution.ts
@@ -1,21 +1,27 @@
 export {}
 
-function canCompleteCircuit(gas: number[], cost: number[]): number {// check each index for valid starting position
-  let totalGas = 0
-  let currentGas = 0
-  let currentStation = 0
+function canCompleteCircuit(gas: number[], cost: number[]): number {
+  // initialize variables to track total gas, current gas, and starting station
+  let totalGas = 0, currentGas = 0, startingStation = 0
 
+  // iterate through each station and keep track of above variables
   for (let i = 0; i < gas.length; i++) {
+    // update gas amounts
     totalGas += gas[i] - cost[i]
     currentGas += gas[i] - cost[i]
 
+    // if the amount of current gas we have is ever below 0 it is impossible route
     if (currentGas < 0) {
+      // reset relevant variables
       currentGas = 0
-      currentStation = i + 1
+      // we know the current station is where route was deemed impossible
+      // so set starting station to next station and continue evaluation 
+      startingStation = i + 1
     }
   }
 
-  return totalGas >= 0 ? currentStation : -1
+  // if total gas is >= 0 circuit is possible, else circuit impossible
+  return totalGas >= 0 ? startingStation : -1
 };
 
 let gas = [1,2,3,4,5]
