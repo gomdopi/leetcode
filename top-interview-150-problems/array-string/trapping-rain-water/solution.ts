f5591bd8b4cbf672040022a8d1693cea560acf6e
gomdopi <io.gomdopi@gmail.com>
2024-04-24 16:27:22 -0700

    redo trapping-rain-water

--- top-interview-150-problems/array-string/trapping-rain-water/solution.ts
+++ top-interview-150-problems/array-string/trapping-rain-water/solution.ts
@@ -2,37 +2,60 @@ export {}
 
 function trap(height: number[]): number {
   const n = height.length
-  if (n <= 2) return 0
-  
+
   let left = 0
   let right = n - 1
-  let maxHeightLeft = height[left]
-  let maxHeightRight = height[right]
-  let rain = 0
+  let leftMaxHeight = height[left]
+  let rightMaxHeight = height[right]
+  let totalRain = 0
 
   while (left < right) {
-    if (maxHeightLeft < maxHeightRight) {
+    if (leftMaxHeight < rightMaxHeight) {
       left++
-      if (height[left] < maxHeightLeft) {
-        rain += maxHeightLeft - height[left]
+      if (height[left] < leftMaxHeight) {
+        totalRain += leftMaxHeight - height[left]
       } else {
-        maxHeightLeft = height[left]
+        leftMaxHeight = height[left]
       }
     } else {
       right--
-      if (height[right] < maxHeightRight) {
-        rain += maxHeightRight - height[right]
+      if (height[right] < rightMaxHeight) {
+        totalRain += rightMaxHeight - height[right]
       } else {
-        maxHeightRight = height[right]
+        rightMaxHeight = height[right]
       }
     }
   }
 
-  return rain
+  return totalRain
 };
 
 let height = [0,1,0,2,1,0,1,3,2,1,2,1]
 console.log(trap(height))
 
 height = [4,2,0,3,2,5]
+console.log(trap(height))
+
+height = [0]
+console.log(trap(height))
+
+height = [1]
+console.log(trap(height))
+
+height = [1000]
+console.log(trap(height))
+
+height = [10,0,0,0,0,0,0,0,0,10]
+console.log(trap(height))
+
+height = [2,0,0,0,0,0,0,0,0,10]
+console.log(trap(height))
+
+height = [1,0,1,0,1,0,1,0,1]
+console.log(trap(height))
+
+height = [0,1,0,1,0,1,0,1]
+console.log(trap(height))
+
+height = [1,0,1,0,1,0,1,0]
 console.log(trap(height))
