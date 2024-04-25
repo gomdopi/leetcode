efa3a7efe78b844c49b0053d53fb6c6272d42388
gomdopi <io.gomdopi@gmail.com>
2024-04-24 17:59:38 -0700

    faster solution for roman-to-integer
    iterate starting from the right

--- top-interview-150-problems/array-string/roman-to-integer/solution.ts
+++ top-interview-150-problems/array-string/roman-to-integer/solution.ts
@@ -22,56 +22,22 @@ function getIntegerValueOfRomanNumeral(c: RomanNumeral): number {
 }
 
 function romanToInt(s: string): number {
-  if (s.length === 1) {
-    return getIntegerValueOfRomanNumeral(s[0] as RomanNumeral)
-  }
-
   let total = 0
 
-  // iterate char by char
-  for (let i = 0; i < s.length;) {
-    let currValue = 0
-
-    // if last char then return its integer value
-    console.log(i)
-    console.log(s.length - 1)
-    if (i === s.length - 1) {
-      currValue = getIntegerValueOfRomanNumeral(s[i++] as RomanNumeral)
+  // iterate char by char from the back
+  for (let i = s.length - 1; i >= 0; i--) {
+    let currentValue = getIntegerValueOfRomanNumeral(s[i] as RomanNumeral)
+    // if previous char was of a higher value
+    if (getIntegerValueOfRomanNumeral(s[i + 1] as RomanNumeral) > currentValue) {
+      // subtract from running total
+      total -= currentValue
     }
-    // else if next char is same
-    else if (s[i] === s[i + 1]) {
-      let count = 2
-
-      // then count repetition
-      while (s[i] === s[i + count] && count < 3) { // can have a max of 4 repetitions
-        count++
-      }
-
-      // and compute integer value
-      currValue = getIntegerValueOfRomanNumeral(s[i] as RomanNumeral) * count
-
-      // and set index to i + count
-      i += count
+    // else add to running total
+    else {
+      total += currentValue
     }
-    // else if next char is higher value char
-    else if (getIntegerValueOfRomanNumeral(s[i] as RomanNumeral) < getIntegerValueOfRomanNumeral(s[i + 1] as RomanNumeral)) {
-      // then compute integer value of combination of both chars
-      currValue = getIntegerValueOfRomanNumeral(s[i + 1] as RomanNumeral) - getIntegerValueOfRomanNumeral(s[i] as RomanNumeral)
-
-      // and set index to i + 2
-      i += 2
-    }
-    // else if next char is lower value char
-    else if (getIntegerValueOfRomanNumeral(s[i] as RomanNumeral) > getIntegerValueOfRomanNumeral(s[i + 1] as RomanNumeral)) {
-      // then compute integer value of current char and set index to i + 1
-      currValue = getIntegerValueOfRomanNumeral(s[i++] as RomanNumeral)
-    }
-
-    // add computed integer value to running total
-    total += currValue
   }
-    
-  // return total
+
   return total
 };
 
