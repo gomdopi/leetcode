564962a35762e8cedd512e8a29f182f1e2ad4de8
gomdopi <io.gomdopi@gmail.com>
2024-04-24 18:09:08 -0700

    slightly faster solution for roman-to-integer
    made it a bit faster again by moving object map outside of method so it isn't created on every function call

--- top-interview-150-problems/array-string/roman-to-integer/solution.ts
+++ top-interview-150-problems/array-string/roman-to-integer/solution.ts
@@ -1,15 +1,16 @@
 export {}
 
+const romanIntegerMap: Record<string, number> = {
+  'I': 1,
+  'V': 5,
+  'X': 10,
+  'L': 50,
+  'C': 100,
+  'D': 500,
+  'M': 1000,
+}
+
 function romanToInt(s: string): number {
-  const romanIntegerMap: Record<string, number> = {
-    'I': 1,
-    'V': 5,
-    'X': 10,
-    'L': 50,
-    'C': 100,
-    'D': 500,
-    'M': 1000,
-  }
 
   let total = 0
 
