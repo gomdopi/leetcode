38e447f128fa7276be57f2d013b5a904e012653f
gomdopi <io.gomdopi@gmail.com>
2024-04-28 22:58:11 -0700

    redo reverse-words-in-a-string without array methods

--- top-interview-150-problems/array-string/reverse-words-in-a-string/solution.ts
+++ top-interview-150-problems/array-string/reverse-words-in-a-string/solution.ts
@@ -1,8 +1,30 @@
 export {}
 
 function reverseWords(s: string): string {
-  return s.split(' ').filter(w => w).reverse().join(' ')
+  let i = s.length - 1
+  let reverse = ''
+
+  while (i >= 0) {
+    if (s[i] != ' ') {
+      let word = s[i]
+
+      while (i > 0 && s[--i] != ' ') {
+        word = s[i] + word
+      }
+
+      reverse += reverse.length === 0 ? word : ' ' + word
+    }
+
+    while (i >= 0 && s[--i] == ' ') {
+    }
+  }
+
+  return reverse
 };
+
+// function reverseWords(s: string): string {
+//   return s.split(' ').filter(w => w).reverse().join(' ')
+// };
 
 let s = "the sky is blue"
 console.log(reverseWords(s))
