14d8adf667835717f65463a282f90c7764ab345f
gomdopi <io.gomdopi@gmail.com>
2024-05-02 16:22:39 -0700

    learned solution for merge-sorted-array
    not using built-in methods

--- top-interview-150-problems/array-string/merge-sorted-array/solution.ts
+++ top-interview-150-problems/array-string/merge-sorted-array/solution.ts
@@ -6,14 +6,38 @@ export {}
 function merge(nums1: number[], m: number, nums2: number[], n: number): void {
   if (n === 0) return
 
-  let i = m // idx tracking nums1
-  let j = 0 // idx tracking nums2
+  let i = m - 1 // idx tracking nums1 valid positions
+  let j = n - 1 // idx tracking nums2
+  let k = m + n - 1 //idx tracking nums1
 
-  while (i < m + n) {
-    nums1[i++] = nums2[j++]
+  // start from end of both arrays and go until both arrays reach their respective starts
+  while (i >= 0 || j >= 0) {
+    // if no numbers left to sort in nums1
+    if (i < 0) {
+      // add remaining nums2 into nums1 starting from k
+      while (j >= 0) {
+        nums1[k--] = nums2[j--]
+        // if we just sorted first of nums2; return
+        if (j === -1) return
+      }
+    }
+    // else if no numbers left to sort in nums2
+    else if (j < 0) {
+      // return since nums1 is already sorted
+      return
+    }
+    // else put relevant nums in their respective positions
+    else {
+      // if nums1Num > nums2Num; move nums1Num to kth position of nums1; update k and i
+      if (nums1[i] > nums2[j]) {
+        nums1[k--] = nums1[i--]
+      }
+      // else nums2Num >= nums1Num; move nums2Num to kth position of nums1; update k and j
+      else {
+        nums1[k--] = nums2[j--]
+      }
+    }
   }
-
-  nums1.sort((a, b) => a - b)
 }
 
 let nums1 = [1, 2, 3, 0, 0, 0]
