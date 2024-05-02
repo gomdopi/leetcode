39d4c15d7564174e64b882d4a43147f0134fb806
gomdopi <io.gomdopi@gmail.com>
2024-05-01 20:33:49 -0700

    self iterative solution for same-tree

--- top-interview-150-problems/binary-tree-general/same-tree/solution.ts
+++ top-interview-150-problems/binary-tree-general/same-tree/solution.ts
@@ -4,8 +4,43 @@ function isSameTree(p: TreeNode | null, q: TreeNode | null): boolean {
   if (!p && !q) return true
   if (!p || !q || p.val != q.val) return false
 
-  // recursive
-  return isSameTree(p.left, q.left) && isSameTree(p.right, q.right)
+  // // recursive
+  // return isSameTree(p.left, q.left) && isSameTree(p.right, q.right)
+
+  // iterative
+  let queueP = [p]
+  let queueQ = [q]
+
+  while (queueP.length > 0 && queueQ.length > 0) {
+    const nodeP = queueP.shift()
+    const nodeQ = queueQ.shift()
+
+    if (nodeP && nodeQ) {
+      if (nodeP.val != nodeQ.val) return false
+
+      if (nodeP.left && nodeQ.left) {
+        if (nodeP.left.val != nodeQ.left.val) return false
+
+        queueP.push(nodeP.left)
+        queueQ.push(nodeQ.left)
+      } else if (!nodeP.left && !nodeQ.left) {
+      } else {
+        return false
+      }
+
+      if (nodeP.right && nodeQ.right) {
+        if (nodeP.right.val != nodeQ.right.val) return false
+
+        queueP.push(nodeP.right)
+        queueQ.push(nodeQ.right)
+      } else if (!nodeP.right && !nodeQ.right) {
+      } else {
+        return false
+      }
+    }
+  }
+
+  return true
 }
 
 let p = new TreeNode(
