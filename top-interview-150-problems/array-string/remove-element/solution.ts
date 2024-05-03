6a725911a2dd927630e487f22922180a65de48c7
gomdopi <io.gomdopi@gmail.com>
2024-05-02 17:36:34 -0700

    learned cleaner solution for remove-element

--- top-interview-150-problems/array-string/remove-element/solution.ts
+++ top-interview-150-problems/array-string/remove-element/solution.ts
@@ -4,31 +4,10 @@ function removeElement(nums: number[], val: number): number {
   const n = nums.length
   if (n === 0 || val > 50) return n
 
-  let i = 0 // idx tracking nums from start
-  let j = n - 1 // idx tracking nums from end
+  let i = 0 // idx tracking number of elements != val
 
-  // while there are numbers that haven't been checked
-  while (i <= j) {
-    if (i === j) {
-      // if current num equals val don't count it; else count it
-      return nums[i] === val ? i : i + 1
-    }
-    // if current number = val then swap with the first number closest to the end != val
-    if (nums[i] === val) {
-      while (j > i && nums[j] === val) {
-        j--
-        // if j = i we're done
-        if (j === i) {
-          return i
-        }
-      }
-      nums[i++] = nums[j]
-      nums[j--] = val
-    }
-    // else just increment i
-    else {
-      i++
-    }
+  for (const n of nums) {
+    if (n != val) nums[i++] = n
   }
 
   return i
