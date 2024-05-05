c50d1ee64e38ae1c75f57a957bdcbd18c41df35b
gomdopi <io.gomdopi@gmail.com>
2024-05-04 18:02:49 -0700

    redo learned iterative solution for symmetric-tree
    use morris algorithm

--- top-interview-150-problems/binary-tree-general/symmetric-tree/solution.ts
+++ top-interview-150-problems/binary-tree-general/symmetric-tree/solution.ts
@@ -6,36 +6,57 @@ function isSymmetric(root: TreeNode | null): boolean {
   // // recursive
   // return isMirror(root.left, root.right)
 
-  // iterative
-  if (!root.left && !root.right) return true
-  else if (!root.left || !root.right) return false
+  // // iterative
+  // if (!root.left && !root.right) return true
+  // else if (!root.left || !root.right) return false
 
-  let stack = [root.left, root.right]
+  // let stack = [root.left, root.right]
 
-  while (stack.length > 0) {
-    const right = stack.pop()
-    const left = stack.pop()
+  // while (stack.length > 0) {
+  //   const right = stack.pop()
+  //   const left = stack.pop()
 
-    if (left.val != right.val) return false
+  //   if (left.val != right.val) return false
 
-    if (left.left && right.right) {
-      stack.push(left.left)
-      stack.push(right.right)
-    } else if (!left.left && !right.right) {
-    } else {
+  //   if (left.left && right.right) {
+  //     stack.push(left.left)
+  //     stack.push(right.right)
+  //   } else if (!left.left && !right.right) {
+  //   } else {
+  //     return false
+  //   }
+
+  //   if (left.right && right.left) {
+  //     stack.push(left.right)
+  //     stack.push(right.left)
+  //   } else if (!left.right && !right.left) {
+  //   } else {
+  //     return false
+  //   }
+  // }
+
+  // return true
+
+  // iterative - morris algorithm
+  let left = root.left
+  let right = root.right
+
+  while (left || right) {
+    const leftMorris = morris(left)
+    const rightMorris = inverseMorris(right)
+
+    if (
+      leftMorris.val !== rightMorris.val ||
+      leftMorris.steps !== rightMorris.steps
+    ) {
       return false
     }
 
-    if (left.right && right.left) {
-      stack.push(left.right)
-      stack.push(right.left)
-    } else if (!left.right && !right.left) {
-    } else {
-      return false
-    }
+    left = leftMorris.current
+    right = rightMorris.current
   }
 
-  return true
+  return left === right
 }
 
 function isMirror(p: TreeNode | null, q: TreeNode | null): boolean {
@@ -43,6 +64,76 @@ function isMirror(p: TreeNode | null, q: TreeNode | null): boolean {
   if (!p || !q || p.val != q.val) return false
 
   return isMirror(p.left, q.right) && isMirror(p.right, q.left)
+}
+
+function morris(root: TreeNode | null): {
+  current: TreeNode | null
+  val: number
+  steps: number
+} {
+  let curr = root
+  let currentSteps = 0
+
+  while (curr) {
+    currentSteps++
+
+    if (!curr.left) {
+      return {
+        current: curr.right,
+        val: curr.val,
+        steps: currentSteps
+      }
+    } else {
+      let last = curr.left
+
+      while (last.right) {
+        last = last.right
+        currentSteps++
+      }
+
+      last.right = curr
+      const temp = curr.left
+      curr.left = null
+      curr = temp
+    }
+  }
+
+  return { current: null, val: null, steps: currentSteps }
+}
+
+function inverseMorris(root: TreeNode | null): {
+  current: TreeNode | null
+  val: number
+  steps: number
+} {
+  let curr = root
+  let currentSteps = 0
+
+  while (curr) {
+    currentSteps++
+
+    if (!curr.right) {
+      return {
+        current: curr.left,
+        val: curr.val,
+        steps: currentSteps
+      }
+    } else {
+      let last = curr.right
+
+      while (last.left) {
+        last = last.left
+        currentSteps++
+      }
+
+      last.left = curr
+      const temp = curr.right
+      curr.right = null
+      curr = temp
+    }
+  }
+
+  return { current: null, val: null, steps: currentSteps }
 }
 
 let root = new TreeNode(
