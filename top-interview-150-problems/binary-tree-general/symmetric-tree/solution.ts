66a48906ab4dda10371054512471cb2fc42c1135
gomdopi <io.gomdopi@gmail.com>
2024-05-04 17:33:43 -0700

    redo self iterative solution for symmetric-tree

--- top-interview-150-problems/binary-tree-general/symmetric-tree/solution.ts
+++ top-interview-150-problems/binary-tree-general/symmetric-tree/solution.ts
@@ -3,8 +3,39 @@ import TreeNode from '../TreeNode'
 function isSymmetric(root: TreeNode | null): boolean {
   if (!root) return true
 
-  // recursive
-  return isMirror(root.left, root.right)
+  // // recursive
+  // return isMirror(root.left, root.right)
+
+  // iterative
+  if (!root.left && !root.right) return true
+  else if (!root.left || !root.right) return false
+
+  let stack = [root.left, root.right]
+
+  while (stack.length > 0) {
+    const right = stack.pop()
+    const left = stack.pop()
+
+    if (left.val != right.val) return false
+
+    if (left.left && right.right) {
+      stack.push(left.left)
+      stack.push(right.right)
+    } else if (!left.left && !right.right) {
+    } else {
+      return false
+    }
+
+    if (left.right && right.left) {
+      stack.push(left.right)
+      stack.push(right.left)
+    } else if (!left.right && !right.left) {
+    } else {
+      return false
+    }
+  }
+
+  return true
 }
 
 function isMirror(p: TreeNode | null, q: TreeNode | null): boolean {
