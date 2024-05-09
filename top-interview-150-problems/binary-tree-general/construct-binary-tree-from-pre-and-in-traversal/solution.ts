7300f9ce0f35657e453c54add1b7e075d797fb6e
gomdopi <io.gomdopi@gmail.com>
2024-05-09 15:54:09 -0700

    learned solution for construct-binary-tree
    preorder and inorder traversal

--- top-interview-150-problems/binary-tree-general/construct-binary-tree-from-pre-and-in-traversal/solution.ts
+++ top-interview-150-problems/binary-tree-general/construct-binary-tree-from-pre-and-in-traversal/solution.ts
@@ -1,22 +1,50 @@
 import TreeNode from '../TreeNode'
 
 function buildTree(preorder: number[], inorder: number[]): TreeNode | null {
-  // base case
-  if (!inorder.length) return null
+  // // recursive
+  // // base case
+  // if (!inorder.length) return null
+  // // build root of tree
+  // let root = new TreeNode(preorder.shift())
+  // // determine inorder array of left and right subtrees
+  // let rootIndex = inorder.indexOf(root.val)
+  // let inorderLeft = inorder.slice(0, rootIndex)
+  // let inorderRight = inorder.slice(rootIndex + 1)
+  // // build left and right subtrees
+  // root.left = buildTree(preorder, inorderLeft)
+  // root.right = buildTree(preorder, inorderRight)
+  // return root
 
-  // build root of tree
-  let root = new TreeNode(preorder.shift())
+  // iterative
+  const n = preorder.length
+  let preIdx = 1
+  let inIdx = 0
 
-  // determine inorder array of left and right subtrees
-  let rootIndex = inorder.indexOf(root.val)
-  let inorderLeft = inorder.slice(0, rootIndex)
-  let inorderRight = inorder.slice(rootIndex + 1)
+  let tree = new TreeNode(preorder[0])
+  let parents: TreeNode[] = []
+  let curr = tree
 
-  // build left and right subtrees
-  root.left = buildTree(preorder, inorderLeft)
-  root.right = buildTree(preorder, inorderRight)
+  while (preIdx < n) {
+    while (preorder[preIdx - 1] !== inorder[inIdx]) {
+      parents.push(curr)
+      curr.left = new TreeNode(preorder[preIdx++])
+      curr = curr.left
+    }
+    inIdx++
+    while (
+      parents.length > 0 &&
+      parents[parents.length - 1].val === inorder[inIdx]
+    ) {
+      inIdx++
+      curr = parents.pop()
+    }
+    if (preIdx < n) {
+      curr.right = new TreeNode(preorder[preIdx++])
+      curr = curr.right
+    }
+  }
 
-  return root
+  return tree
 }
 
 let preorder = [3, 9, 20, 15, 7]
@@ -33,4 +61,16 @@ console.log(buildTree(preorder, inorder))
 
 preorder = [1, 2, 3]
 inorder = [3, 2, 1]
+console.log(buildTree(preorder, inorder))
+
+preorder = [1, 2, 3]
+inorder = [1, 2, 3]
+console.log(buildTree(preorder, inorder))
+
+preorder = [1, 12, 5, 6, 3, 2, 9]
+inorder = [5, 12, 6, 3, 2, 1, 9]
+console.log(buildTree(preorder, inorder))
+
+preorder = [1, 2, 3, 4, 5, 7, 8]
+inorder = [5, 4, 3, 2, 1, 7, 8]
 console.log(buildTree(preorder, inorder))
