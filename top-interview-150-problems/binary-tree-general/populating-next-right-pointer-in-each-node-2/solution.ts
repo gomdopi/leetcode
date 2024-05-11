c13e15ca997d36dc5d166a35629eed891878e3f9
gomdopi <io.gomdopi@gmail.com>
2024-05-10 17:57:49 -0700

    learned solution for populating-next-right-pointers
    recursive and iterative

---
+++ top-interview-150-problems/binary-tree-general/populating-next-right-pointer-in-each-node-2/solution.ts
@@ -0,0 +1,78 @@
+import { Node } from '../TreeNode'
+
+function connect(root: Node | null): Node | null {
+  // // recursive
+  // helper(root)
+  // return root
+
+  // iterative
+  if (!root) return null
+
+  let q: Node[] = []
+  q.push(root)
+
+  // while there are nodes
+  while (q.length > 0) {
+    const qLen = q.length
+
+    // add the subtrees of the nodes already present in the queue
+    for (let i = 0; i < qLen; i++) {
+      const node = q.shift()
+
+      if (node.left) {
+        q.push(node.left)
+      }
+      if (node.right) {
+        q.push(node.right)
+      }
+    }
+
+    // connect the nodes that were just added
+    for (let i = 0; i < q.length - 1; i++) {
+      q[i].next = q[i + 1]
+    }
+  }
+
+  return root
+}
+
+function findNext(node: Node | null): Node | null {
+  if (!node) {
+    return null
+  }
+  if (node.left) {
+    return node.left
+  }
+  if (node.right) {
+    return node.right
+  }
+  return findNext(node.next)
+}
+
+function helper(root: Node | null): Node | null {
+  if (!root) {
+    return null
+  }
+  if (root.left) {
+    if (root.right) {
+      root.left.next = root.right
+    } else {
+      root.left.next = findNext(root.next)
+    }
+  }
+  if (root.right) {
+    root.right.next = findNext(root.next)
+  }
+  helper(root.right)
+  helper(root.left)
+}
+
+let root = new Node(
+  1,
+  new Node(2, new Node(4), new Node(5)),
+  new Node(3, null, new Node(7))
+)
+console.log(connect(root))
+
+root = null
+console.log(connect(root))
