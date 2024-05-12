de55b244ec9bba4a6c51a3db37f374cb3c8fd1e8
gomdopi <io.gomdopi@gmail.com>
2024-05-11 20:15:11 -0700

    redo learned recursive populating-next-right-pointer

--- top-interview-150-problems/binary-tree-general/populating-next-right-pointer-in-each-node-2/solution.ts
+++ top-interview-150-problems/binary-tree-general/populating-next-right-pointer-in-each-node-2/solution.ts
@@ -1,55 +1,50 @@
 import { Node } from '../TreeNode'
 
 function connect(root: Node | null): Node | null {
-  // // recursive
-  // helper(root)
-  // return root
-
-  // iterative
-  if (!root) return null
-
-  let q: Node[] = []
-  q.push(root)
-
-  while (q.length > 0) {
-    const qLen = q.length
-
-    for (let i = 0; i < qLen; i++) {
-      const node = q.shift()
-
-      if (node.left) {
-        q.push(node.left)
-      }
-      if (node.right) {
-        q.push(node.right)
-      }
-    }
-
-    for (let i = 0; i < q.length - 1; i++) {
-      q[i].next = q[i + 1]
-    }
-  }
-
+  // recursive
+  helper(root)
   return root
+
+  // // iterative
+  // if (!root) return null
+  // let q: Node[] = []
+  // q.push(root)
+  // while (q.length > 0) {
+  //   const qLen = q.length
+  //   for (let i = 0; i < qLen; i++) {
+  //     const node = q.shift()
+  //     if (node.left) {
+  //       q.push(node.left)
+  //     }
+  //     if (node.right) {
+  //       q.push(node.right)
+  //     }
+  //   }
+  //   for (let i = 0; i < q.length - 1; i++) {
+  //     q[i].next = q[i + 1]
+  //   }
+  // }
+  // return root
 }
 
 function findNext(node: Node | null): Node | null {
   if (!node) {
     return null
   }
+
   if (node.left) {
     return node.left
   }
   if (node.right) {
     return node.right
   }
+
   return findNext(node.next)
 }
 
 function helper(root: Node | null): Node | null {
-  if (!root) {
-    return null
-  }
+  if (!root) return null
+
   if (root.left) {
     if (root.right) {
       root.left.next = root.right
@@ -60,6 +55,7 @@ function helper(root: Node | null): Node | null {
   if (root.right) {
     root.right.next = findNext(root.next)
   }
+
   helper(root.right)
   helper(root.left)
 }
