d21e9d1589f7c14cb30c8445baac3c7ade7d1176
gomdopi <io.gomdopi@gmail.com>
2024-05-13 15:44:52 -0700

    redo recursive populating-next-right-pointer

--- top-interview-150-problems/binary-tree-general/populating-next-right-pointer-in-each-node-2/solution.ts
+++ top-interview-150-problems/binary-tree-general/populating-next-right-pointer-in-each-node-2/solution.ts
@@ -3,42 +3,40 @@ import { Node } from '../TreeNode'
 function connect(root: Node | null): Node | null {
   if (!root) return null
 
-  // // recursive
-  // helper(root)
-  // return root
-
-  // iterative
-  let q: Node[] = [] // use queue for FIFO order
-  q.push(root)
-
-  while (q.length > 0) {
-    const qLen = q.length // store length of q here at this point in time so value doesn't change in for check below
-
-    // push child nodes to "q" starting with left node first
-    for (let i = 0; i < qLen; i++) {
-      const node = q.shift()
-
-      if (node.left) {
-        q.push(node.left)
-      }
-      if (node.right) {
-        q.push(node.right)
-      }
-    }
-
-    // establish connections for nodes of the same level
-    for (let i = 0; i < q.length; i++) {
-      q[i].next = q[i + 1]
-    }
-  }
-
+  // recursive
+  helper(root)
   return root
+
+  // // iterative
+  // let q: Node[] = [] // use queue for FIFO order
+  // q.push(root)
+
+  // while (q.length > 0) {
+  //   const qLen = q.length // store length of q here at this point in time so value doesn't change in for check below
+
+  //   // push child nodes to "q" starting with left node first
+  //   for (let i = 0; i < qLen; i++) {
+  //     const node = q.shift()
+
+  //     if (node.left) {
+  //       q.push(node.left)
+  //     }
+  //     if (node.right) {
+  //       q.push(node.right)
+  //     }
+  //   }
+
+  //   // establish connections for nodes of the same level
+  //   for (let i = 0; i < q.length; i++) {
+  //     q[i].next = q[i + 1]
+  //   }
+  // }
+
+  // return root
 }
 
 function findNext(node: Node | null): Node | null {
-  if (!node) {
-    return null
-  }
+  if (!node) return null
 
   if (node.left) {
     return node.left
@@ -54,11 +52,7 @@ function helper(root: Node | null): Node | null {
   if (!root) return null
 
   if (root.left) {
-    if (root.right) {
-      root.left.next = root.right
-    } else {
-      root.left.next = findNext(root.next)
-    }
+    root.left.next = root.right ? root.right : findNext(root.next)
   }
   if (root.right) {
     root.right.next = findNext(root.next)
