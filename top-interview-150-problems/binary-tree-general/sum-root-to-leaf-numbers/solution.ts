11e1496ac5bddc489d814920f4cee8c06245679c
gomdopi <io.gomdopi@gmail.com>
2024-05-16 13:21:03 -0700

    learned recursive sum-root-to-leaf-numbers

--- top-interview-150-problems/binary-tree-general/sum-root-to-leaf-numbers/solution.ts
+++ top-interview-150-problems/binary-tree-general/sum-root-to-leaf-numbers/solution.ts
@@ -3,68 +3,47 @@ import TreeNode from '../TreeNode'
 function sumNumbers(root: TreeNode | null): number {
   if (!root) return 0
 
-  // // recursive
-  // let digits = ''
+  // recursive
+  return helper(root, '', 0)
 
-  // let rtlNumbers = helper(root, digits, [])
-
+  // // iterative
+  // let stack: TreeNode[] = []
   // let sum = 0
 
-  // for (const n of rtlNumbers) {
-  //   sum += n
+  // stack.push(root)
+
+  // while (stack.length > 0) {
+  //   const node = stack.pop()
+
+  //   if (!node.left && !node.right) {
+  //     sum += node.val
+  //   }
+
+  //   if (node.left) {
+  //     node.left.val = Number(node.val.toString() + node.left.val.toString())
+  //     stack.push(node.left)
+  //   }
+  //   if (node.right) {
+  //     node.right.val = Number(node.val.toString() + node.right.val.toString())
+  //     stack.push(node.right)
+  //   }
   // }
 
   // return sum
-
-  // iterative
-  let stack: TreeNode[] = []
-  let sum = 0
-
-  stack.push(root)
-
-  while (stack.length > 0) {
-    const node = stack.pop()
-
-    if (!node.left && !node.right) {
-      sum += node.val
-    }
-
-    if (node.left) {
-      node.left.val = Number(node.val.toString() + node.left.val.toString())
-      stack.push(node.left)
-    }
-    if (node.right) {
-      node.right.val = Number(node.val.toString() + node.right.val.toString())
-      stack.push(node.right)
-    }
-  }
-
-  return sum
 }
 
-function helper(
-  root: TreeNode | null,
-  digits: string,
-  rtlNumbers: number[]
-): number[] {
-  digits += root.val.toString()
-
-  if (!root.left && !root.right) {
-    rtlNumbers.push(Number(digits))
-
-    return rtlNumbers
-  } else {
-    if (root.left && root.right) {
-      return [
-        ...helper(root.left, digits, []),
-        ...helper(root.right, digits, [])
-      ]
-    } else if (root.left) {
-      return helper(root.left, digits, rtlNumbers)
-    } else {
-      return helper(root.right, digits, rtlNumbers)
-    }
+function helper(node: TreeNode | null, currValue: string, sum: number): number {
+  if (!node) {
+    return sum
   }
+
+  currValue += node.val.toString()
+
+  if (!node.left && !node.right) {
+    return sum + Number(currValue)
+  }
+
+  return helper(node.left, currValue, sum) + helper(node.right, currValue, sum)
 }
 
 let root = new TreeNode(1, new TreeNode(2), new TreeNode(3))
