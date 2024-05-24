d3a77631a3c1e36fa7d12d323a0aff9212f607b1
gomdopi <io.gomdopi@gmail.com>
2024-05-24 15:07:56 -0700

    learned iterative number-of-islands

--- top-interview-150-problems/graph-general/number-of-islands/solution.ts
+++ top-interview-150-problems/graph-general/number-of-islands/solution.ts
@@ -3,14 +3,44 @@ export {}
 function numIslands(grid: string[][]): number {
   let islands = 0
 
-  grid.forEach((row, rowIdx) => {
-    row.forEach((ele, colIdx) => {
-      if (ele === '1') {
-        islands++
-        explore(grid, rowIdx, colIdx)
+  // // recursive
+
+  // grid.forEach((row, rowIdx) => {
+  //   row.forEach((ele, colIdx) => {
+  //     if (ele === '1') {
+  //       islands++
+  //       explore(grid, rowIdx, colIdx)
+  //     }
+  //   })
+  // })
+
+  // return islands
+
+  // iterative
+  const stack: number[][] = []
+  const m: number = grid.length
+  const n: number = grid[0].length
+
+  for (let i = 0; i < m; i++) {
+    for (let j = 0; j < n; j++) {
+      if (grid[i][j] !== '1') continue
+
+      islands++
+      stack.push([i, j])
+
+      while (stack.length > 0) {
+        const [i, j] = stack.pop()
+        if (grid[i][j] !== '1') continue
+
+        grid[i][j] = '2' // marking cell as visited
+
+        grid[i + 1] && grid[i + 1][j] === '1' && stack.push([i + 1, j])
+        grid[i - 1] && grid[i - 1][j] === '1' && stack.push([i - 1, j])
+        grid[i] && grid[i][j + 1] === '1' && stack.push([i, j + 1])
+        grid[i] && grid[i][j - 1] === '1' && stack.push([i, j - 1])
       }
-    })
-  })
+    }
+  }
 
   return islands
 }
@@ -31,5 +61,13 @@ let grid = [
   ['1', '1', '0', '1', '0'],
   ['1', '1', '0', '0', '0'],
   ['0', '0', '0', '0', '0']
+]
+console.log(numIslands(grid))
+
+grid = [
+  ['1', '1', '0', '0', '0'],
+  ['1', '1', '0', '0', '0'],
+  ['0', '0', '1', '0', '0'],
+  ['0', '0', '0', '1', '1']
 ]
 console.log(numIslands(grid))
