b8741c9a23dede6221fc4b5df106f9019ce327a4
gomdopi <io.gomdopi@gmail.com>
2024-05-29 15:23:43 -0700

    redo learned iterative number-of-islands

--- top-interview-150-problems/graph-general/number-of-islands/solution.ts
+++ top-interview-150-problems/graph-general/number-of-islands/solution.ts
@@ -3,22 +3,46 @@ export {}
 function numIslands(grid: string[][]): number {
   let islands = 0
 
-  // recursive
-  grid.forEach((row, rowIdx) => {
-    row.forEach((ele, colIdx) => {
-      if (ele === '1') {
+  // // recursive
+  // grid.forEach((row, rowIdx) => {
+  //   row.forEach((ele, colIdx) => {
+  //     if (ele === '1') {
+  //       islands++
+  //       explore(rowIdx, colIdx, grid)
+  //     }
+  //   })
+  // })
+
+  // iterative
+  let stack: number[][] = []
+
+  for (let i = 0; i < grid.length; i++) {
+    for (let j = 0; j < grid[0].length; j++) {
+      if (grid[i][j] === '1') {
         islands++
-        explore(rowIdx, colIdx, grid)
+        stack.push([i, j])
       }
-    })
-  })
+
+      while (stack.length > 0) {
+        const [x, y] = stack.pop()
+
+        if (grid[x]?.[y] === '1') {
+          grid[x][y] = '2' // mark as visited
+          stack.push([x + 1, y])
+          stack.push([x - 1, y])
+          stack.push([x, y + 1])
+          stack.push([x, y - 1])
+        }
+      }
+    }
+  }
 
   return islands
 }
 
 function explore(x: number, y: number, grid: string[][]) {
   if (grid[x]?.[y] === '1') {
-    grid[x][y] = '0'
+    grid[x][y] = '0' // mark as visited
     explore(x + 1, y, grid)
     explore(x - 1, y, grid)
     explore(x, y + 1, grid)
