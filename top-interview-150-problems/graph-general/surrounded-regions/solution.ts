ad7aa98fdc2f767b4ccfd3e00dc484ec69c91e02
gomdopi <io.gomdopi@gmail.com>
2024-05-31 18:10:29 -0700

    redo iterative surrounded-regions
    inspired by learned recursive solution

--- top-interview-150-problems/graph-general/surrounded-regions/solution.ts
+++ top-interview-150-problems/graph-general/surrounded-regions/solution.ts
@@ -7,34 +7,84 @@ function solve(board: string[][]): void {
   const m = board.length
   const n = board[0].length
 
-  // recursive
-  function explore(x: number, y: number): void {
-    if (x < 0 || x >= m || y < 0 || y >= n || board[x][y] !== 'O') return
+  // // recursive
+  // function explore(x: number, y: number): void {
+  //   if (x < 0 || x >= m || y < 0 || y >= n || board[x][y] !== 'O') return
 
-    board[x][y] = 'U'
-    explore(x + 1, y)
-    explore(x - 1, y)
-    explore(x, y + 1)
-    explore(x, y - 1)
-  }
+  //   board[x][y] = 'U'
+  //   explore(x + 1, y)
+  //   explore(x - 1, y)
+  //   explore(x, y + 1)
+  //   explore(x, y - 1)
+  // }
+
+  // // only need to find 'O's on the border since only they can be not surrounded
+  // // top
+  // for (let i = 0; i < n; i++) {
+  //   if (board[0][i] === 'O') explore(0, i)
+  // }
+  // // bot
+  // for (let i = 0; i < n; i++) {
+  //   if (board[m - 1][i] === 'O') explore(m - 1, i)
+  // }
+  // // don't need to check corners since they were checked already above
+  // // left
+  // for (let i = 1; i < m - 1; i++) {
+  //   if (board[i][0] === 'O') explore(i, 0)
+  // }
+  // // right
+  // for (let i = 1; i < m - 1; i++) {
+  //   if (board[i][n - 1] === 'O') explore(i, n - 1)
+  // }
+
+  // // we've updated all 'O's that are not surrounded to 'U'
+  // // so update all remaining 'O's to 'X'
+  // for (let i = 0; i < m; i++) {
+  //   for (let j = 0; j < n; j++) {
+  //     if (board[i][j] === 'O') board[i][j] = 'X'
+  //   }
+  // }
+
+  // // change all 'U's back to 'O'
+  // for (let i = 0; i < m; i++) {
+  //   for (let j = 0; j < n; j++) {
+  //     if (board[i][j] === 'U') board[i][j] = 'O'
+  //   }
+  // }
+
+  // iterative
+  let stack: number[][] = []
 
   // only need to find 'O's on the border since only they can be not surrounded
   // top
   for (let i = 0; i < n; i++) {
-    if (board[0][i] === 'O') explore(0, i)
+    if (board[0][i] === 'O') stack.push([0, i])
   }
   // bot
   for (let i = 0; i < n; i++) {
-    if (board[m - 1][i] === 'O') explore(m - 1, i)
+    if (board[m - 1][i] === 'O') stack.push([m - 1, i])
   }
   // don't need to check corners since they were checked already above
   // left
   for (let i = 1; i < m - 1; i++) {
-    if (board[i][0] === 'O') explore(i, 0)
+    if (board[i][0] === 'O') stack.push([i, 0])
   }
   // right
   for (let i = 1; i < m - 1; i++) {
-    if (board[i][n - 1] === 'O') explore(i, n - 1)
+    if (board[i][n - 1] === 'O') stack.push([i, n - 1])
+  }
+
+  // update all border 'O's to 'U' and find connecting 'O's
+  while (stack.length > 0) {
+    const [x, y] = stack.pop()
+
+    if (x < 0 || x >= m || y < 0 || y >= n || board[x][y] !== 'O') continue
+
+    board[x][y] = 'U'
+    stack.push([x + 1, y])
+    stack.push([x - 1, y])
+    stack.push([x, y + 1])
+    stack.push([x, y - 1])
   }
 
   // we've updated all 'O's that are not surrounded to 'U'
